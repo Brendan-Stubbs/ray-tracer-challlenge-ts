35cb15827ea83e227c127d3cb00a2ddcbf6ed54a
Brendan-Stubbs <brendan@root.co.za>
2025-02-10 18:59:05 +0200

    refactored matrix

--- src/matrix.ts
+++ src/matrix.ts
@@ -1,50 +1,25 @@
-export class Matrix {
-  public data: number[][];
-  constructor(data: number[][]) {
-    if (data.length === 0 || data[0].length === 0) {
-      throw new Error("Matrix cannot have empty rows or columns.");
-    }
+export type Matrix = number[][];
 
-    this.data = data;
-  }
+export const get = (m: Matrix, row: number, col: number): number => {
+  return m[row][col];
+};
 
-  get(row: number, col: number): number {
-    return this.data[row][col];
-  }
-
-  equals(other: Matrix): boolean {
-    if (this.data.length !== other.data.length) {
-      return false;
-    }
-
-    if (this.data[0].length !== other.data[0].length) {
-      return false;
-    }
-
-    for (let i = 0; i < this.data.length; i++) {
-      for (let j = 0; j < this.data[0].length; j++) {
-        if (this.data[i][j] !== other.data[i][j]) {
-          return false;
-        }
-      }
-    }
-
-    return true;
-  }
-}
+export const equals = (a: Matrix, b: Matrix): boolean => {
+  return JSON.stringify(a) === JSON.stringify(b);
+};
 
 export const matrixMultiply = (a: Matrix, b: Matrix): Matrix => {
-  const result: number[][] = Array.from({ length: a.data.length }, () =>
-    new Array(b.data[0].length).fill(0)
+  const result: number[][] = Array.from({ length: a.length }, () =>
+    new Array(b[0].length).fill(0)
   );
 
-  for (let i = 0; i < a.data.length; i++) {
-    for (let j = 0; j < a.data[0].length; j++) {
-      for (let k = 0; k < result[0].length; k++) {
-        result[i][j] += a.data[i][k] * b.data[k][j];
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < b[0].length; j++) {
+      for (let k = 0; k < a[0].length; k++) {
+        result[i][j] += a[i][k] * b[k][j];
       }
     }
   }
 
-  return new Matrix(result);
+  return result;
 };
