1466cd83e81bf0e09d34dcfa49115b09174f1aa5
Brendan-Stubbs <brendan@root.co.za>
2025-03-15 08:45:21 +0200

    completed chapter 3

--- tests/matrix.test.ts
+++ tests/matrix.test.ts
@@ -314,4 +314,62 @@ describe("Matrix", () => {
     const result = inverse(a);
     expect(equals(result, expectedResult)).toBe(true);
   });
+
+  it("Calculating the inverse of another matrix", () => {
+    const input = [
+      [8, -5, 9, 2],
+      [7, 5, 6, 1],
+      [-6, 0, 9, 6],
+      [-3, 0, -9, -4],
+    ];
+
+    const expectedResult = [
+      [-0.15385, -0.15385, -0.28205, -0.53846],
+      [-0.07692, 0.12308, 0.02564, 0.03077],
+      [0.35897, 0.35897, 0.4359, 0.92308],
+      [-0.69231, -0.69231, -0.76923, -1.92308],
+    ];
+
+    const result = inverse(input);
+    expect(equals(result, expectedResult)).toBe(true);
+  });
+
+  it("Calculating the inverse of a third matrix", () => {
+    const input = [
+      [9, 3, 0, 9],
+      [-5, -2, -6, -3],
+      [-4, 9, 6, 4],
+      [-7, 6, 6, 2],
+    ];
+
+    const expectedResult = [
+      [-0.04074, -0.07778, 0.14444, -0.22222],
+      [-0.07778, 0.03333, 0.36667, -0.33333],
+      [-0.02901, -0.1463, -0.10926, 0.12963],
+      [0.17778, 0.06667, -0.26667, 0.33333],
+    ];
+
+    const result = inverse(input);
+    expect(equals(result, expectedResult)).toBe(true);
+  });
+
+  it("Multiplying a product by its inverse", () => {
+    const a = [
+      [3, -9, 7, 3],
+      [3, -8, 2, -9],
+      [-4, 4, 4, 1],
+      [-6, 5, -1, 1],
+    ];
+
+    const b = [
+      [8, 2, 2, 2],
+      [3, -1, 7, 0],
+      [7, 0, 5, 4],
+      [6, -2, 0, 5],
+    ];
+
+    const c = matrixMultiply(a, b);
+    const result = matrixMultiply(c, inverse(b));
+    expect(equals(a, result)).toBe(true);
+  });
 });
